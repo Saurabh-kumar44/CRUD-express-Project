261a37f57cf4e74adde9758163ef5511c49b2073
saurav <sauravvermas1097v@gmail.com>
2023-05-01 22:23:07 +0530

    crud app

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3001;
+import web from './routes/web.js';
+// app.use('/', web);
+
+//Database connection
+import connectdb from './db/connectdb';
+const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017';
+connectdb(DATABASE_URL);
+
+//static file
+import {join} from 'path';
+
+app.use('/', web);
+app.use('/',express.static(join(process.cwd(), 'public')));
+app.use('/edit',express.static(join(process.cwd(), 'public')));
+
+//Loading middleware(urlencoded) which is used to parse incomming requests
+// app.use(express.urlencoded({ extended: false} ));
+
+//Setting up engine
+app.set('view engine', 'ejs');
+
+
+app.listen(port, ()=>{
+    console.log("Listening to port",port);
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+});
